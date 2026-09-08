2328c25c9ce03151ec11f1e34c607fa361a7f37e
lichao-bigbigli <bigbigli0926@gmail.com>
2026-09-08 18:06:57 +0800

    Update config.ts

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -128,7 +128,7 @@ export default defineConfig({
       // ],
       '/48-lessons/': [
         {
-          text: '',
+          text: '看前须知',
           collapsed: false,
           items: [
             { text: '已出版：购买实体书', link: '/48-lessons/index' },
