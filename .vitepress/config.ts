c7b5a9ab7451ca1e2d5bc593e25f9ecf2b8832c1
lichao-bigbigli <bigbigli0926@gmail.com>
2026-09-09 18:30:48 +0800

    Update config.ts

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
   ignoreDeadLinks: true,
   
   head: [
-    ['link', { rel: 'icon', type: 'image/png', href: '/docs/image.png' }]
+    ['link', { rel: 'icon', type: 'image/png', href: 'theme/img/image.png' }]
   ],
 
   themeConfig: {
